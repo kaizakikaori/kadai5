154e7f8c238d58a28aa56b4ebaf6ee6f081afbd5
VERTEX21 <vertex21@VERTEX21noMacBook-Air.local>
2019-08-16 04:58:03 +0900

    first commit

---
+++ UserListDay5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserListDay5
+//
+//  Created by VERTEX21 on 2019/08/16.
+//  Copyright © 2019 k-kougi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
